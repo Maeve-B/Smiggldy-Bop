278716ab51140cf58a311a411e0d4aa160448e42
Maeve-B <56639638+Maeve-B@users.noreply.github.com>
2019-10-16 14:28:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function multiplyBy() {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    ans= document.getElementById("ans").innerHTML = num1 * num2;
+}
+
+
+
+function divideBy() {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    ans = document.getElementById("ans").innerHTML = num1 / num2;
+}
+
+
+
+
+
+
+
+//var num1,num2;
+//var sum = num1 + num2; 
+
+//next, add the input buttons
+
+//<input type="button" value="Click me" onclick="msg()">
+
+//var num1 = document.getElementById("num1");
+//var num2 = document.getElementById("num2");
+
+//document.write("num1");
+            
+    
+    
+
+
+
+
+
+
+//<input type="text" id="Number1">
+  //  <var num1 = document.getElementById("Number1");
+    
+    
+
+      
+       // <button onclick="multiplyFunction()"> Multiply Button</button>
+       // <button onclick="divideFunction()"> Divide Button </button>
+      
+
+    //function multiplyFunction() {
+       // var ans = 5;
+        //var num1 = Number1;
+        //var ans = document.getElementById("Number1" * "Number2");
+        //document.write(ans);
+        //document.write(num1);
+        
+
+    
