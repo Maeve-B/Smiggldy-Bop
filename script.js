b44fc8d03c6371bc2fb553f961b9dd6224030dba
Maeve-B <56639638+Maeve-B@users.noreply.github.com>
2019-10-17 11:04:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function multiplyBy(num1, num2) {
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    ans= document.getElementById("ans").innerHTML = num1 * num2;
-}
-
-
-
-function divideBy() {
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    ans = document.getElementById("ans").innerHTML = num1 / num2;
-}
-
-//plan need to add function for multiply, divide, square, 
+//var display = document.getElementById("display");
+//
+//function inputted(obj) {
+//
+//var pushed = obj.innerHTML;
+//
+//if (pushed == '=') {
+//    // Calculate
+//    inputLabel.innerHTML = eval(inputted.innerHTML);
+//
+//} else if (pushed == 'AC') {
+//    // All Clear
+//    inputted.innerHTML = '0';
+//
+//} else {
+//    if (inputted.innerHTML == '0') {
+//        inputted.innerHTML = pushed;
+//
+//    } else {
+//        inputted.innerHTML += pushed;   
         
 
+function doTheMaths(result) {
+		form.displayResult.value=form.displayResult.value+result;
+}
     
